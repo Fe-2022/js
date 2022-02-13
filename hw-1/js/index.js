103713a9d41b21c6e35036e6fb4d94c29e3956cb
Plaha-dev <plaha.dv@gmail.com>
2022-02-13 15:24:29 +0200

    hw1

---
+++ hw-1/js/index.js
@@ -0,0 +1,178 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+// - Переприсвоїти кожній змінній з завдання значення на довільне.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+//     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+// - Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+//     В однакових виразах не використовувати однакові оператори!!!
+//     5 ? 6 -> true
+// 5 ? 6 -> false
+// 5 ? 6 -> false
+// 5 ? 6 -> false
+// 10 ? 10 -> true
+// 10 ? 10 -> true
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 123 ? '123' -> false
+// 123 ? '123' -> true
+//
+// - Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+//     let str = "20";
+// let a = 5;
+// document.write(str + a + "<br/>");
+// document.write(str - a + "<br/>");
+// document.write(str * "2" + "<br/>");
+// document.write(str / 2 + "<br/>");
+
+
+
+
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+// - Переприсвоїти кожній змінній з завдання значення на довільне.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+// let hello ' ;
+// hello = 'привіт';
+// console.log('hello');
+// document.write('hello');
+// alert('hello');
+
+
+// let owu;
+//  owu = 'октен';
+// console.log('owu');
+// document.write('owu');
+// alert('owu');
+
+
+// let com;
+// com = 'домен';
+// console.log('домен');
+// document.write('домен');
+// alert('домен');
+
+// let ua;
+// ua = 'Україна';
+// console.log('Україна');
+// document.write('Україна');
+// alert('Україна');
+
+
+//
+// let sum = 1;
+//  sum = 'один';
+//  console.log(sum);
+//  document.write(sum);
+//  alert(sum);
+
+
+// let num = 10;
+// num = 'десять';
+// console.log(num);
+// document.write(num);
+// alert(num);
+
+
+// let xxx = -999;
+// xxx = 'минус девять девять девять'
+// console.log(xxx);
+// document.write(xxx);
+// alert('xxx');
+
+
+// let bbb = 123;
+// bbb = 'раз два три';
+// console.log(bbb);
+// document.write(bbb);
+// alert('bbb');
+
+
+// let Pi = 3.14;
+// Pi = 'Пи';
+// console.log(Pi);
+// document.write(Pi);
+// alert(Pi);
+
+
+// let yar = 2.7;
+// yar = 'два и семь';
+// console.log(yar);
+// document.write(yar);
+// alert(yar);
+//
+// let red = 16;
+// red = 'шестнадцать';
+// console.log(red);
+// document.write(red);
+// alert(red);
+//
+// let arrOne = true;
+// arrOne = false;
+// console.log(arrOne);
+// document.write(arrOne);
+// alert(arrOne);
+
+// let arrTwo = false;
+// arrTwo = true;
+// console.log(arrTwo);
+// document.write(arrTwo);
+// alert(arrTwo);
+
+//Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+// // - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+// //     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+//
+// let firstName= prompt('Данил')
+// let middleName= prompt('Валерьевич')
+// let lastName=prompt('Плахотин')
+// let age=prompt('27')
+// console.log(firstName);
+// document.write('Вітаю' + '' + firstName +'' + middleName +'. Тобі '+age +' років' );
+//
+//
+// //За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+// //     let a = 100; let b = '100'; let c = true;
+//
+//
+//     let h = 100;
+// console.log(typeof h);
+//
+// let j ='100';
+// console.log(typeof j);
+//
+// let m =true;
+// console.log(typeof m);
+//
+//
+//
+// // - Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+// //     В однакових виразах не використовувати однакові оператори!!!
+// //     5 ? 6 -> true
+// // 5 ? 6 -> false
+// // 5 ? 6 -> false
+// // 5 ? 6 -> false
+// // 10 ? 10 -> true
+// // 10 ? 10 -> true
+// // 10 ? 10 -> false
+// // 10 ? 10 -> false
+// // 10 ? 10 -> false
+// // 123 ? '123' -> false
+// // 123 ? '123' -> true
+//
+// console.log(5!=6);
+// console.log(5>6);
+// console.log(5==6);
+// console.log(5>=6);
+// console.log(10==10);
+// console.log(10===10);
+// console.log(10>10);
+// console.log(10!==10);
+// console.log(10<10);
+// console.log(123 > '123');
+// console.log(123 !== '123');
